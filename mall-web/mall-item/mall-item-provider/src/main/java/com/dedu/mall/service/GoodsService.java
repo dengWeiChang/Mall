f1eed1d982254f7a96f60dcfaf4663a59defca51
Android <swpuxzh@163.com>
2019-09-29 23:08:36 +0800

    【mall-web】商品Service

---
+++ mall-web/mall-item/mall-item-provider/src/main/java/com/dedu/mall/service/GoodsService.java
@@ -0,0 +1,6 @@
+package com.dedu.mall.service;
+
+import com.dedu.mall.model.mysql.BrandVo;
+
+public interface GoodsService {
+}
---
+++ mall-web/mall-item/mall-item-provider/src/main/java/com/dedu/mall/service/impl/GoodsSerImpl.java
@@ -0,0 +1,11 @@
+package com.dedu.mall.service.impl;
+
+import com.dedu.mall.service.GoodsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class GoodsSerImpl implements GoodsService {
+
+}
