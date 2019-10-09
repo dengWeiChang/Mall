bff35a3ce52ad5ba146345bff325d3a75bea8abc
Android <swpuxzh@163.com>
2019-10-09 22:04:54 +0800

    【mall-web】订单详情SQL

--- mall-web/mall-order/mall-order-provider/src/main/java/com/dedu/mall/dao/OrderMapper.java
+++ mall-web/mall-order/mall-order-provider/src/main/java/com/dedu/mall/dao/OrderMapper.java
@@ -16,7 +16,7 @@ public interface OrderMapper extends BaseMapper<OrderPo> {
     @Select("SELECT COUNT(*) FROM tb_order o LEFT JOIN tb_order_status s ON o.id = s.order_id LEFT JOIN tb_pay p ON o.id = p.order_id")
     Integer getOrderAllInfoCount();
 
-    @Select("SELECT o.id as id,o.buyer_id as buyerId, o.total_price as totalPrice, o.title as title, o.create_time as createTime, o.update_time as updateTime, s.status as status, p.total_pay as totalPay, p.pay_type as payType FROM tb_order o LEFT JOIN tb_order_status s ON o.id = s.order_id LEFT JOIN tb_pay p ON o.id = p.order_id WHERE o.id = #{orderId}")
+    @Select("SELECT o.id as id,o.buyer_id as buyerId,o.num as num, o.sourceType as sourceType,o.total_price as totalPrice, o.title as title, o.create_time as createTime, o.update_time as updateTime, o.logistics_name as logisticsName,  o.logistics_code as logisticsCode, o.receiver_state as receiverState, o.receiver_city as receiverCity, o.receiver_district as receiverDistrict, o.receiver_address as receiverAddress, o.receiver_mobile as receiverMobile, o.receiver_postcode as receiverPostcode, o.receiver_name as receiverName, s.status as status, s.consign_time as consignTime, s.end_time as endTime, s.close_time as closeTime, s.comment_time as commentTime, s.create_time as statusCreateTime, s.update_time as statusUpdateTime, p.total_pay as totalPay, p.pay_type as payType, p.status as payStatus, p.pay_time as payTime, p.closed_time as payClosedTime, p.refund_time as refundTime FROM tb_order o LEFT JOIN tb_order_status s ON o.id = s.order_id LEFT JOIN tb_pay p ON o.id = p.order_id WHERE o.id = #{orderId}")
     OrderDetailPo getOrderDetailInfoById(@Param(value = "orderId") Long id);
 
 }
--- mall-web/mall-order/mall-order-provider/src/main/java/com/dedu/mall/model/mysql/OrderDetailPo.java
+++ mall-web/mall-order/mall-order-provider/src/main/java/com/dedu/mall/model/mysql/OrderDetailPo.java
@@ -1,4 +1,36 @@
 package com.dedu.mall.model.mysql;
 
 public class OrderDetailPo {
+    private Long id;
+    private Long buyerId;
+    private Integer num;
+    private BigDecimal totalPrice;
+    private String title;
+    private Integer sourceType;
+    private String logisticsName;
+    private String logisticsCode;
+    private String receiverState;
+    private String receiverCity;
+    private String receiverDistrict;
+    private String receiverAddress;
+    private String receiverMobile;
+    private String receiverPostcode;
+    private String receiverName;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+
+    private Integer status;
+    private LocalDateTime consignTime;
+    private LocalDateTime endTime;
+    private LocalDateTime closeTime;
+    private LocalDateTime commentTime;
+    private LocalDateTime statusCreateTime;
+    private LocalDateTime statusUpdateTime;
+
+    private BigDecimal totalPay;
+    private Integer payStatus;
+    private Integer payType;
+    private LocalDateTime payTime;
+    private LocalDateTime payClosedTime;
+    private LocalDateTime refundTime;
 }
