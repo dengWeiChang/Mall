27f0a537e9a2251054791e2ac032e8738bebccf8
dedu18 <swpuxzh@163.com>
2019-10-25 14:45:04 +0800

    【mall-cart】跨域注解

--- mall-web/mall-cart/src/main/java/com/dedu/mall/controller/CartController.java
+++ mall-web/mall-cart/src/main/java/com/dedu/mall/controller/CartController.java
@@ -7,11 +7,9 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
